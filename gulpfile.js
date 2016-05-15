dc6aa5e88c005301290b71666bc3ae8760865e4e
Lea Verou <lea@verou.me>
2016-05-14 19:53:49 -0700

    Fix gulpfile, change talks title

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,13 @@
-
 var gulp  = require("gulp");
-var copy = require("gulp-copy");
-
-var wysieFiles = ["../wysie/wysie.min.js", "../wysie/wysie.css"];
+var copy  = require("gulp-copy");
 
 gulp.task("update", function() {
-	var files = "wysie permissions storage node unit expression functions scope primitive primitive.imgur collection prettyprint debug storage.dropbox storage.github"
-	            .split(" ").map(path => "src/" + path + ".js");
-	files.unshift("../bliss/bliss.js");
-	files.unshift("../stretchy/stretchy.js");
-
-	return gulp.src(wysieFiles).pipe(copy("."));
+	gulp.src(["../wysie/wysie.min.js"]).pipe(gulp.dest("."));
+	gulp.src(["../wysie/wysie.css"]).pipe(gulp.dest("css"));
 });
 
 gulp.task("watch", function() {
-	gulp.watch(wysieFiles, ["update"]);
+	gulp.watch(["../wysie/wysie.min.js", "../wysie/wysie.css"], ["update"]);
 });
 
 gulp.task("default", ["update"]);
