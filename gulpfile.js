5f7f1ec5d45b78f730ce8e22ad5b7ea9e47a650f
Lea Verou <lea@verou.me>
2018-01-21 12:26:44 -0500

    Added PostCSS and converted CSS to use nesting. Moved inline CSS from Publications to stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const rename = require("gulp-rename");
 const fileinclude = require("gulp-file-include");
+const postcss = require('gulp-postcss');
 
 gulp.task("html", function() {
 	return gulp.src(["**/*.tpl.html"])
@@ -14,8 +15,28 @@ gulp.task("html", function() {
 		.pipe(gulp.dest("."))
 });
 
+gulp.task('css', function () {
+	return gulp.src(["**/*.src.css", "!node_modules/**"])
+		.pipe(postcss([
+			require('postcss-nesting')(),
+			require("postcss-selector-matches")({
+				lineBreak: true
+			}),
+			require('autoprefixer')({
+				browsers: ["last 3 years"]
+			}),
+			require("postcss-custom-properties")({
+				preserve: true,
+				warnings: false
+			})
+		]))
+		.pipe(rename({ extname: "" }))
+		.pipe(rename({ extname: ".css" }))
+		.pipe(gulp.dest('.'));
+});
+
 gulp.task("watch", function() {
-	//gulp.watch(["**/*.src.css"], ["css"]);
+	gulp.watch(["**/*.src.css"], ["css"]);
 	gulp.watch(["**/*.tpl.html", "./templates/*.html"], ["html"]);
 });
 
